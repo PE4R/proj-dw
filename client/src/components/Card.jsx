79538c6ec92de69b70bd4d1a20ac87ea6d9c2486
PE4R <ricardoduarte@live.com.pt>
2024-01-14 22:17:11 +0000

    Dynamic cards

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -4,18 +4,21 @@ function Card(props){
     return(
         <div className='card'>
             <img className='card-image' src={props.src} alt={props.alt} />
+            <h3 className='card-title'>{props.name}</h3>
             <p className='card-text'>{props.description}</p>
         </div>
     )
 }
 
 Card.propTypes = {
+    name: PropTypes.string,
     src: PropTypes.string,
     alt: PropTypes.string,
     description: PropTypes.string,
 }
 
 Card.defaultProps = {
+    name: "default name",
     src: "https://images.adsttc.com/media/images/5b3b/6d3a/f197/cc2d/c700/0064/large_jpg/a-132.jpg",
     alt: "default image",
     description: "description",
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -1,12 +1,33 @@
+import { useState, useEffect } from 'react'
+import axios from 'axios'
 import Card from '../components/Card.jsx'
 
 function Home(){
+    const [accommodations, setAccommodations] = useState([])
     
+    useEffect(() => {
+        const fetchAccommodations = async () => {
+            try {
+                const response = await axios.get('/api/accommodations')
+                setAccommodations(response.data)
+            } catch (err) {
+                console.error('Error fetching accommodations:', err)
+            }
+        }
+        fetchAccommodations()
+    }, [])
+
     return(
         <div className="content">
-            <Card />
-            <Card />
-            <Card />
+            {accommodations.map(accommodation => (
+                <Card
+                    key={accommodation.id}
+                    name={accommodation.name}
+                    src={accommodation.image_url}
+                    alt={`Image of ${accommodation.name}`}
+                    description={accommodation.description}
+                />
+            ))}
         </div>
     )
 }
